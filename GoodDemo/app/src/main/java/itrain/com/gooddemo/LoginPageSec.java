708f50d774ed3eb63637a57847d2516e903473e7
idatafox <wishlong@yahoo.com>
2017-04-14 21:42:57 +0800

    2017.4.14

---
+++ GoodDemo/app/src/main/java/itrain/com/gooddemo/LoginPageSec.java
@@ -0,0 +1,13 @@
+package itrain.com.gooddemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginPageSec extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_page_sec);
+    }
+}
